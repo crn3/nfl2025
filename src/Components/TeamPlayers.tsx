fc7672d90b0f210b519adee04ab8082f173be603
Rachael Kenny <157695390+crn3@users.noreply.github.com>
2025-12-06 19:54:29 +0000

    Complete TeamPlayers

--- src/Components/TeamPlayers.tsx
+++ src/Components/TeamPlayers.tsx
@@ -35,22 +35,42 @@ function TeamPlayers() {
 
   const [players, setPlayers] = useState<TeamPlayer[]>([]);
 
+  const handleWeight = (weightPounds: number) => {
+    const stone = Math.floor(weightPounds / 14);
+    const pounds = weightPounds % 14;
+    return `${stone}st ${pounds}lbs `;
+  };
+
+  const handleHeight = (heightInches: number) => {
+    const feet = Math.floor(heightInches / 12);
+    const inches = heightInches % 12;
+    return `${feet}' ${inches}" `;
+  };
+
+  const handleAge = (jsonDate:string) => {
+    //https://www.w3resource.com/javascript-exercises/javascript-date-exercise-18.php
+    const diff_ms = Date.now() - Date.parse(jsonDate);
+    const age_dt = new Date(diff_ms);
+    const birthdateFormatted = new Date(jsonDate).toISOString().split("T")[0];
+    const age = Math.abs(age_dt.getUTCFullYear() - 1970);
+    return `${birthdateFormatted} (${age})`;
+    
+  }
+
   useEffect(() => {
     const getPlayers = async () => {
       const response = await axios.get<TeamPlayer[]>(
         `http://localhost:3000/players/${teamAbbreviation}`
       );
-      console.log(response);
-      console.log({ teamAbbreviation });
       const alphabetical = [...response.data].sort((a, b) =>
         a.lastName.localeCompare(b.lastName)
       );
       setPlayers(alphabetical);
-      console.log(alphabetical);
     };
 
     getPlayers();
   }, [teamAbbreviation]);
+
   return (
     <>
       <h1>{teamName}</h1>
@@ -76,21 +96,12 @@ function TeamPlayers() {
               <td>{player.teamDisplayName}</td>
               <td>{player.position}</td>
               <td>
-                {(() => {
-                  const stone = Math.floor(player.weight / 14);
-                  const pounds = player.weight % 14;
-                  return `${stone}st ${pounds}lbs `;
-                })()}
-                ({player.weight}lbs)
+                {handleWeight(player.weight)}({player.weight}lbs)
               </td>
               <td>
-                {(() => {
-                  const feet = Math.floor(player.height / 12);
-                  const inches = player.height % 12;
-                  return `${feet}' ${inches}" `;
-                })()}
-                ({player.height}")</td>
-              <td>{player.dateOfBirth}</td>
+                {handleHeight(player.height)}({player.height}")
+              </td>
+              <td>{handleAge(player.dateOfBirth)}</td>
               <td>{player.college}</td>
             </tr>
           ))}
